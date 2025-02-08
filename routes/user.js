600049a08aa75a1a9b1077ae772b2d0cf482de18
pramit-ahir <pramitahir6121@gmail.com>
2025-02-08 18:48:27 +0530

    added user list api

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const Router = express.Router()
-const { addUser, loginUser, updateUser, getUser } = require('./../controllers/user')
-const { addUserValidation, loginValidation, updateUserValidation } = require('./../validation/user')
+const { addUser, loginUser, updateUser, getUser, getUsers } = require('./../controllers/user')
+const { addUserValidation, loginValidation, updateUserValidation, userListValidation } = require('./../validation/user')
 const { verifyToken } = require('./../authentication/index')
 
 Router.post('/', addUserValidation, addUser)
 Router.post('/login', loginValidation, loginUser)
 Router.put('/', verifyToken, updateUserValidation, updateUser)
 Router.get('/', verifyToken, getUser)
+Router.get('/list', verifyToken, userListValidation, getUsers)
 
 module.exports = Router
