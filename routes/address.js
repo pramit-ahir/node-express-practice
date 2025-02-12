79eb094cdda7a06efad20c179e3c40b39a66d446
pramit-ahir <pramitahir6121@gmail.com>
2025-02-12 12:23:35 +0530

    addedd get user address api

--- routes/address.js
+++ routes/address.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const Router = express.Router()
 
-const { addUserAddress } = require('./../controllers/address')
+const { addUserAddress, getUserAddress } = require('./../controllers/address')
 const { addAddressvalidation } = require('./../validation/address')
 const { verifyToken } = require('../authentication')
 
 Router.post('/', verifyToken, addAddressvalidation, addUserAddress)
+Router.get('/', verifyToken, getUserAddress)
 
 module.exports = Router
